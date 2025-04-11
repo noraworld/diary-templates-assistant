f0bb02e63a9350b8701f584398d16e81713435e9
Kosuke Aoki <mail@noraworld.com>
2025-04-11 22:48:50 +0900

    Add ability to specify entire year, month, and day
    like wild card

--- app/day-off.py
+++ app/day-off.py
@@ -12,19 +12,38 @@ WEEKLY_OFF_DAYS = ['Saturday', 'Sunday']
 
 class Util:
     def full_date(date_str):
-        # '2022/05/20' -> ['2022', '05', '20']
-        #      '05/20' ->         ['05', '20']
-        #         '20' ->               ['20']
+        # '2022/05/20' -> [2022, 5, 20]
+        # '2022/05/00' -> [2022, 5,  0]
         splitted_date = list(map(int, re.split(DATE_DELIMITER_REGEXP, date_str)))
 
-        if len(splitted_date) == 3:
-            return datetime.date(splitted_date[0], splitted_date[1], splitted_date[2])
-        elif len(splitted_date) == 2:
-            return datetime.date(datetime.date.today().year, splitted_date[0], splitted_date[1])
-        elif len(splitted_date) == 1:
-            return datetime.date(datetime.date.today().year, datetime.date.today().month, splitted_date[0])
-        else:
+        if len(splitted_date) != 3:
             raise ValueError('Invalid date format')
+
+        # Let's say today is 2025/04/11
+        # [2025, 4, 11] -> 2025
+        # [   0, 4, 11] -> 2025
+        if splitted_date[0] == 0:
+            year = datetime.date.today().year
+        else:
+            year = splitted_date[0]
+
+        # Let's say today is 2025/04/11
+        # [2025, 4, 11] -> 4
+        # [2025, 0, 11] -> 4
+        if splitted_date[1] == 0:
+            month = datetime.date.today().month
+        else:
+            month = splitted_date[1]
+
+        # Let's say today is 2025/04/11
+        # [2025, 4, 11] -> 11
+        # [2025, 4,  0] -> 11
+        if splitted_date[2] == 0:
+            day = datetime.date.today().day
+        else:
+            day = splitted_date[2]
+
+        return datetime.date(year, month, day)
 
 class HolidayError(Exception):
     def __str__(self):
