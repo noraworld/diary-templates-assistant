1bcb549edd6cc02c034a8013db0c5cf06ca88225
Kosuke Aoki <mail@noraworld.com>
2025-08-21 13:28:22 +0900

    Add option to specify date to append or check

--- app/day-off.py
+++ app/day-off.py
@@ -1,6 +1,5 @@
 # cf. https://github.com/noraworld/attendance
 
-import datetime
 import jpholiday
 import json
 import os
@@ -20,18 +19,18 @@ if __name__ == '__main__':
             data = json.load(f)
 
         for day_off in data['day_off']:
-            if datetime.date.today() == Util.full_date(day_off):
+            if Util.full_date(args.date) == Util.full_date(day_off):
                 Util.handle_error(DayOffError)
 
-    if jpholiday.is_holiday(datetime.date.today()) is True:
+    if jpholiday.is_holiday(Util.full_date(args.date)) is True:
         Util.handle_error(HolidayError)
 
-    if (datetime.date.today().strftime('%A') in WEEKLY_OFF_DAYS) is True:
+    if (Util.full_date(args.date).strftime('%A') in WEEKLY_OFF_DAYS) is True:
         Util.handle_error(WeeklyOffError)
 
     match args.operation:
         case 'append':
-            data['day_off'].append(datetime.date.today().strftime("%Y-%m-%d"))
+            data['day_off'].append(Util.full_date(args.date).strftime("%Y-%m-%d"))
             with open(DAY_OFF_JSON, 'w') as f:
                 json.dump(data, f, ensure_ascii=False, indent=2)
                 print('', file=f) # insert a trailing newline
--- lib/day_off/option.py
+++ lib/day_off/option.py
@@ -1,16 +1,29 @@
 from argparse import ArgumentParser
+import datetime
 
 class Option:
     def parse():
         parser = ArgumentParser(description='')
 
-        operation_subparsers = parser.add_subparsers(dest='operation')
-        operation_subparsers.required = True
+        subparsers = parser.add_subparsers(dest='operation')
+        subparsers.required = True
 
-        operation_parsers = []
-        operation_parsers.append(operation_subparsers.add_parser('append', help='Append a specified day to a day off JSON file'))
-        operation_parsers.append(operation_subparsers.add_parser('check', help='Check if a specified day is a day off'))
+        # https://chatgpt.com/s/t_68a6963cc1588191bb5e4fed6a91dda3
+        date_arg = ArgumentParser(add_help=False)
+        date_arg.add_argument(
+            'date',
+            nargs='?',
+            metavar='DATE',
+            default=datetime.date.today().isoformat(),
+            help='A target date to append or check (format: YYYY-MM-DD, default: today)'
+        )
 
-        args = parser.parse_args()
+        commands = [
+            ( 'append', 'Append a specified day to a day off JSON file' ),
+            ( 'check',  'Check if a specified day is a day off' ),
+        ]
 
-        return args
+        for command, help in commands:
+            subparsers.add_parser(command, parents=[date_arg], help=help)
+
+        return parser.parse_args()
